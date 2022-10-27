3bd23247df9d956c79e53678c5012b9b186d6ca5
Taiizor <41683699+Taiizor@users.noreply.github.com>
2022-10-27 18:22:02 +0300

    Initial commit

---
+++ Desktop/Crossui.WF/MainPage.cs
@@ -0,0 +1,29 @@
+﻿using Crossui.Library;
+using Crossui.Library.Data;
+using Microsoft.AspNetCore.Components.WebView.WindowsForms;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Crossui.WF
+{
+    public partial class MainPage : Form
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            ServiceCollection services = new();
+
+            services.AddWindowsFormsBlazorWebView();
+
+            services.AddSingleton<WeatherForecastService>();
+
+#if DEBUG
+            services.AddBlazorWebViewDeveloperTools();
+#endif
+
+            blazorWebView1.HostPage = "wwwroot\\index.html";
+            blazorWebView1.Services = services.BuildServiceProvider();
+            blazorWebView1.RootComponents.Add<Main>("#app");
+        }
+    }
+}
