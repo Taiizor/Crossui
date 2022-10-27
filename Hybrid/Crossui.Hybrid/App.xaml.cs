55f943b46c22c0bb243fa889cc50a8c800db10d5
Taiizor <41683699+Taiizor@users.noreply.github.com>
2022-10-27 17:55:27 +0300

    Initial commit

---
+++ Hybrid/Crossui.Hybrid/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Crossui.Hybrid
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
