ce73155fc61d3cfa823b60a578e9b65e1be9f397
Taiizor <41683699+Taiizor@users.noreply.github.com>
2022-10-27 19:23:23 +0300

    Update AssemblyInfo.cs

--- Console/Crossui.Console/Properties/AssemblyInfo.cs
+++ Console/Crossui.Console/Properties/AssemblyInfo.cs
@@ -1,17 +1,17 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 // Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
 // denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
 // bu öznitelik değerlerini değiştirin.
 [assembly: AssemblyTitle("Crossui.Console")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Crossui.Console")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyProduct("Crossui.Console")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyTrademark("")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyCulture("")]
 
 // Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
@@ -24,6 +24,7 @@ using System.Runtime.InteropServices;
 // Tüm değerleri belirtebilir veya varsayılan Derleme ve Düzeltme Numaralarını kullanmak için 
 // aşağıda gösterildiği gibi '*' kullanabilirsiniz:
 // [derleme: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("en-GB")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
 [assembly: ComVisible(false)]
