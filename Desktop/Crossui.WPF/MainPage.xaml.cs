0d10fd3c33c9648d64fa77e8d65bae08db3ffa87
Taiizor <41683699+Taiizor@users.noreply.github.com>
2022-10-27 18:31:04 +0300

    Initial commit

---
+++ Desktop/Crossui.WPF/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Crossui.Library.Data;
+using Microsoft.Extensions.DependencyInjection;
+using System.Windows;
+
+namespace Crossui.WF
+{
+    /// <summary>
+    /// MainPage.xaml etkileşim mantığı
+    /// </summary>
+    public partial class MainPage : Window
+    {
+        public MainPage()
+        {
+            ServiceCollection serviceCollection = new();
+
+            serviceCollection.AddWpfBlazorWebView();
+
+            serviceCollection.AddSingleton<WeatherForecastService>();
+
+#if DEBUG
+            serviceCollection.AddBlazorWebViewDeveloperTools();
+#endif
+
+            Resources.Add("services", serviceCollection.BuildServiceProvider());
+
+            InitializeComponent();
+        }
+    }
+}
